59118db6a56d7c97df3e4db6fd9de71866b70a44
philipp2310 <41761223+philipp2310@users.noreply.github.com>
2019-01-09 00:10:31 +0100

    Create de.py

---
+++ translations/de.py
@@ -0,0 +1,30 @@
+###
+# German Translation
+# Add inside of the brackets any possible sentence - but be aware: every combination might occure!
+###
+
+### Add items
+# XXX, XXX und XXX wurden zur Einkaufsliste hinzugefügt
+ADD_COMBINE = ["und"]
+ADD_COMPLETE = ["wurden zur Einkaufsliste hinzugefügt"]
+
+#, aber YYY, YYY und YYY waren bereits vorhanden
+ADD_F_START = [", aber"]
+ADD_F_COMBINE = ["und"]
+ADD_F_COMPLETE = ["waren bereits vorhanden"]
+
+#. (close sentence off for correct pronounciation, eg "." or "!")
+ADD_CLOSE = [".", "!"]
+
+### Remove Items
+# XXX, XXX und XXX wurden von der Einkaufsliste gestrichen
+REM_COMBINE = ["und"]
+REM_COMPLETE = ["wurden von der Einkaufsliste gestrichen"]
+
+#, aber YYY, YYY und YYY waren nicht vorhanden
+REM_F_START = [", aber"]
+REM_F_COMBINE = ["und"]
+REM_F_COMPLETE = ["waren nicht vorhanden"]
+
+#. (close sentence off for correct pronounciation, eg "." or "!")
+REM_CLOSE = [".", "!"]
