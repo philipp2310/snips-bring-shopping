8459dadb63544e399eeac8f0a4175da6c45f507b
Philipp <41761223+philipp2310@users.noreply.github.com>
2019-01-10 00:25:55 +0100

    strtemp

--- action-Philipp.bring_shopping.py
+++ action-Philipp.bring_shopping.py
@@ -71,7 +71,7 @@ def text_list(itemlist, lot, one, end):
         response = random.choice(lot).format(first=", ".join(itemlist[:-1]), last=itemlist[-1])
     else:
         response = random.choice(one).format(first=itemlist[0])
-    response = str_temp + random.choice(end)
+    response += random.choice(end)
     return response
 
 
